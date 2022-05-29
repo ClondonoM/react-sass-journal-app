4c6f85191de95fc2f9eaf548708a944a1d17e774
Carlos Londono <c.londono.m@gmail.com>
2022-05-28 20:24:54 -0500

    styles in login sicreen

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,18 +1,42 @@
+import { Link } from 'react-router-dom';
+
 export const LoginScreen = () => {
   return (
     <>
-      <h3>Login</h3>
+      <h3 className='auth__title mb-5'>Login</h3>
       <form>
-        <input className='mt-1' type='text' placeholder='email' name='email' />
         <input
-          className='mt-1'
+          className='auth__input mt-5 mb-5'
+          type='text'
+          placeholder='email'
+          name='email'
+          autocomplete='off'
+        />
+        <input
+          className='auth__input mt-1 mb-5'
           type='password'
           placeholder='password'
           name='password'
         />
-        <button type='submit'>Login</button>
+        <button className='btn btn-primary btn-block mb-1' type='submit'>
+          Login
+        </button>
         <hr />
-        Google
+        <div className='auth__social-networks mb-1'>
+          <div className='google-btn mt-1'>
+            <div className='google-icon-wrapper'>
+              <img
+                className='google-icon'
+                src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
+                alt='google button'
+              />
+            </div>
+            <p className='btn-text'>
+              <b>Sign in with google</b>
+            </p>
+          </div>
+        </div>
+        <Link to='/auth/register'>Create new account</Link>
       </form>
     </>
   );
